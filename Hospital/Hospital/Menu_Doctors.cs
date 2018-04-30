80b4ca4282bfc64e005c11d5ab51197bad8cf49a
Leonardo Costa <dragorelite@gmail.com>
2018-04-30 16:24:01 +0100

    Menus de pacientes e doutores já têm a opção para voltar para o menu inicial.

--- Hospital/Hospital/Menu_Doctors.cs
+++ Hospital/Hospital/Menu_Doctors.cs
@@ -10,11 +10,31 @@ namespace Hospital
     {
         public void Show()
         {
-            Console.WriteLine("1- New Doctor");
-            Console.WriteLine("2- List Doctors");
-            Console.WriteLine("3- Edit Doctor");
-            Console.WriteLine("4- Remove Doctor");
-            Console.WriteLine("5- Return");
+            Console.Clear();
+            int opcao;
+            Menu_Principal menu = new Menu_Principal();
+            Menu_Patient patient = new Menu_Patient();
+            Menu_Doctors doctor = new Menu_Doctors();
+            Menu_Symptoms symptom = new Menu_Symptoms();
+            Menu_Diseases disease = new Menu_Diseases();
+
+            do
+            {
+                Console.WriteLine("1- New Doctor");
+                Console.WriteLine("2- List Doctors");
+                Console.WriteLine("3- Edit Doctor");
+                Console.WriteLine("4- Remove Doctor");
+                Console.WriteLine("0- Return");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.Clear();
+                        menu.Show();
+                        break;
+                }
+            } while (opcao != 0);
         }
     }
 }
--- Hospital/Hospital/Menu_Patient.cs
+++ Hospital/Hospital/Menu_Patient.cs
@@ -10,11 +10,32 @@ namespace Hospital
     {
         public void Show()
         {
-            Console.WriteLine("1- New Patient");
-            Console.WriteLine("2- List Patients");
-            Console.WriteLine("3- Edit Patient");
-            Console.WriteLine("4- Remove Patient");
-            Console.WriteLine("5- Return");
+            Console.Clear();
+            int opcao;
+            Menu_Principal menu = new Menu_Principal();
+            Menu_Patient patient = new Menu_Patient();
+            Menu_Doctors doctor = new Menu_Doctors();
+            Menu_Symptoms symptom = new Menu_Symptoms();
+            Menu_Diseases disease = new Menu_Diseases();
+
+            do
+            {
+                Console.WriteLine("1- New Patient");
+                Console.WriteLine("2- List Patients");
+                Console.WriteLine("3- Edit Patient");
+                Console.WriteLine("4- Remove Patient");
+                Console.WriteLine("0- Return");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.Clear();
+                        menu.Show();
+                        break;
+                }
+            } while (opcao != 0);
         }
     }
 }
